a07082c13c8ec72725a54cbe7ed6d258e836893f
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-08-20 18:06:48 +1000

    Made intro section responsive

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Avatar from '../images/walter.jpeg';
 
 const Intro = () => (
   <section class="intro">
@@ -6,12 +7,20 @@ const Intro = () => (
       <h1 className="section-heading">Who am I?</h1>
     </div>
     <div className="intro-content">
-      <p className="paragraph">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo provident
-        necessitatibus atque quos corporis autem unde amet odit? Expedita dicta
-        est consequuntur ab distinctio quas corrupti inventore sapiente
-        voluptatum rerum!
-      </p>
+      <img src={Avatar} alt="Walter Selvakumar" className="intro-avatar" />
+      <div className="intro-text">
+        <p className="paragraph">
+          Hi there! I'm Walter, a full-stack web developer with a passion for
+          programming. I like learning new technologies and am not shy to delve
+          into new tools and frameworks. I mainly work with JavaScript on, but I
+          can also code in Java and PHP, and I can quickly learn the syntax of a
+          new language.
+        </p>
+        <p class="paragraph">
+          When I'm not furiously typing into Visual Studio Code, I like hanging
+          out with friends, playing chess and table tennis.
+        </p>
+      </div>
     </div>
   </section>
 );
